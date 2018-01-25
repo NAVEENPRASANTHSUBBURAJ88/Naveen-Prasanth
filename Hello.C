c85f633862c8e537274c34236f65db7bbd638336
NAVEENPRASANTHSUBBURAJ88 <35652579+NAVEENPRASANTHSUBBURAJ88@users.noreply.github.com>
2018-01-25 16:10:05 +0530

    Create Hello.C

---
+++ Hello.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,N;
+    printf("Enter N Value\n");
+    scanf("%d",&N);
+    for(i=0;i<N;i++)
+    printf("Hello\n");
+    getch();
+}
+
+
