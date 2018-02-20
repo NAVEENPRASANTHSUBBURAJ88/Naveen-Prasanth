738947b5087e534418cd52dc9b181a5f9b598334
NAVEENPRASANTHSUBBURAJ88 <35652579+NAVEENPRASANTHSUBBURAJ88@users.noreply.github.com>
2018-02-20 15:55:30 +0530

    Create GUVI54.C

---
+++ GUVI54.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+	int n;
+	printf("enter the no:\n");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("the nearest even no is %d",(n-2));
+		
+	}
+	else
+	{
+		printf("the nearest even no is %d",(n-1));
+	}
+	return 0;
+}
