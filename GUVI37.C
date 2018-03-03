cde1e1d53763301f2ecb450fb56a494767d5ab8d
NAVEENPRASANTHSUBBURAJ88 <35652579+NAVEENPRASANTHSUBBURAJ88@users.noreply.github.com>
2018-03-03 16:03:34 +0530

    Update GUVI37.C

--- GUVI37.C
+++ GUVI37.C
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-int a,b,t;
+int m,n,t;
 printf("Enter 2 numbers\n");
-scanf("%d %d",&a,&b);
-printf("Before Swapping %d %d\n",a,b);
-t=a;
-a=b;
-b=t;
-printf("After Swapping %d %d",a,b);
+scanf("%d %d",&m,&n);
+printf("Before Swapping %d %d\n",m,n);
+t=m;
+m=n;
+n=t;
+printf("After Swapping %d %d",m,n);
 return 0;
 }
