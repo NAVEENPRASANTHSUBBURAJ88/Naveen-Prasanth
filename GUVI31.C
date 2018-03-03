87fb52cc27a7d2da087f9b954fe402ce66df53ad
NAVEENPRASANTHSUBBURAJ88 <35652579+NAVEENPRASANTHSUBBURAJ88@users.noreply.github.com>
2018-03-03 14:02:26 +0530

    Update GUVI31.C

--- GUVI31.C
+++ GUVI31.C
@@ -1,10 +1,12 @@
 #include<stdio.h>
+#include<conio.h>
 void main()
 {
     char a[100];
+    clrscr();
     int i,num=0;
     printf("Enter the string\n");
-    scanf("%[^\n]",&a);
+    scanf("%[^\n]s",&a);
     for(i=0;a[i]!='\0';i++)
     {
         if(a[i]!=' ')
@@ -13,4 +15,5 @@ void main()
         }
     }
     printf("Number of characters is %d",num);
+    getch();
 }
