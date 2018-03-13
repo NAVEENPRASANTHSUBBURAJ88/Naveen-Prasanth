92c3d928f9759f4c5befa68f5fdb89d26360d325
NAVEENPRASANTHSUBBURAJ88 <35652579+NAVEENPRASANTHSUBBURAJ88@users.noreply.github.com>
2018-03-13 16:15:34 +0530

    Create GUVI26.C

---
+++ GUVI26.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,c,a[50];
+    printf("Enter size of array\n");
+    scanf("%d",&n);
+    printf("Enter the elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                c=a[i];
+                a[i]=a[j];
+                a[j]=c;
+            }
+        
+        }
+    }
+    printf("The Sorted elements are\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    
+    printf("The middle element is %d",a[(n-1)/2]);
+    return 0;
+}
