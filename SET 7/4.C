9808e4c6023a787c9a75daa54566373ade15cd2e
NAVEENPRASANTHSUBBURAJ88 <35652579+NAVEENPRASANTHSUBBURAJ88@users.noreply.github.com>
2018-03-16 14:20:56 +0530

    Create 4.C

---
+++ SET 7/4.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("Enter the numbers \n");
+scanf("%d \n %d",&a,&b);
+c=a+b;
+if(c%2==0)
+{
+    printf("Even");
+}
+else
+{
+    printf("Odd");
+}
+return 0;
+}
